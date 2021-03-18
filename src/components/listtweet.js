82ab021fb504fb929349daf30dbe82fcf2b9de88
adanmc01 <dev.adanmc01@gmail.com>
2021-03-18 12:54:47 -0600

    fix tweet messagge

--- src/components/listtweet.js
+++ src/components/listtweet.js
@@ -6,13 +6,13 @@ const List = ({issue, mps, setShowForm}) => {
     console.log(mps)
     switch (issue) {
         case '1' :
-            tweetText = `${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I+think+our+taxes+are+too+high%21+Australians+deserve+to+keep+more+of+their+own+money+and+I%E2%80%99m+asking+you+to+fight+in+Canberra+for+lower+taxes.+%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
+            tweetText = `.${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I+think+our+taxes+are+too+high%21+Australians+deserve+to+keep+more+of+their+own+money+and+I%E2%80%99m+asking+you+to+fight+in+Canberra+for+lower+taxes.+%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
             break
         case '2':
-            tweetText = `${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I+want+you+to+fight+in+Canberra+for+fewer+regulations.+Small+businesses+and+the+middle+class+are+the+backbones+of+our+economy.%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
+            tweetText = `.${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I+want+you+to+fight+in+Canberra+for+fewer+regulations.+Small+businesses+and+the+middle+class+are+the+backbones+of+our+economy.%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
             break
         case '3':
-            tweetText = `${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I%E2%80%99m+very+concerned+about+wasteful+spending.+Taxpayer+dollars+are+being+wasted+on+unnecessary+programs.+I%E2%80%99m+asking+you+to+fight+for+a+better+use+of+our+money%21%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
+            tweetText = `.${mps.twitter}%2C+I%E2%80%99m+a+voter+in+your+electorate%2C+and+I%E2%80%99m+very+concerned+about+wasteful+spending.+Taxpayer+dollars+are+being+wasted+on+unnecessary+programs.+I%E2%80%99m+asking+you+to+fight+for+a+better+use+of+our+money%21%0D%0A%0D%0ASent+from+%40AusTaxpayers%E2%80%99+Contact+Your+Politician+Platform.&original_referer=https://www.taxpayers.org.au/let-them-know-you-care`
             break
     }
     return (
